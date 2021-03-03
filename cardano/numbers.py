9eb9131e0803a64becf008fa1ecc39e71fd42ff0
Michał Sałaban <michal@salaban.info>
2021-03-03 14:28:36 +0100

    Add warning on floats

--- cardano/numbers.py
+++ cardano/numbers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from warnings import warn
 
 LOVELACE = Decimal("0.000001")
 
@@ -10,6 +11,11 @@ def to_lovelaces(amount):
             "Amount '{}' doesn't have numeric type. Only Decimal, int and "
             "float (not recommended) are accepted as amounts."
         )
+    if isinstance(amount, float):
+        warn("to_lovelaces() received amount of float type ({:f}). It is STRONGLY DISCOURAGED "
+            "to use floating-point numbers for monetary operations due to rounding errors".format(
+                amount),
+            RuntimeWarning)
     return int(amount * 10 ** 6)
 
 
@@ -20,4 +26,9 @@ def from_lovelaces(amount):
 
 def as_ada(amount):
     """Return the amount rounded to maximal ADA precision."""
+    if isinstance(amount, float):
+        warn("as_ada() received amount of float type ({:f}). It is STRONGLY DISCOURAGED "
+            "to use floating-point numbers for monetary operations due to rounding errors".format(
+                amount),
+            RuntimeWarning)
     return Decimal(amount).quantize(LOVELACE)
