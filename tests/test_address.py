827e0200bf2989f525fcb77fc5a1d394073b533d
Michał Sałaban <michal@salaban.info>
2021-03-10 18:01:31 +0100

    Add tests on addresses

--- tests/test_address.py
+++ tests/test_address.py
@@ -10,8 +10,8 @@ class TestAddress(unittest.TestCase):
         addr = Address(ADDR)
         self.assertEqual(addr.era, Era.SHELLEY)
 
-    def test_invalid_shelley(self):     # this is known to FAIL
-        ADDR = "addr1v8fet8gavr6elqt6q50XXXXf025zthqu6vr56l5k39sp9aqlvz2g4"
+    def test_invalid(self):
+        ADDR = "XXX"
         with self.assertRaises(ValueError):
             Address(ADDR)
 
@@ -22,6 +22,11 @@ class TestComparisons(unittest.TestCase):
         addr2 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
         self.assertEqual(addr1, addr2)
 
+    def test_cmp_different_address(self):
+        addr1 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
+        addr2 = Address("addr1XXX") #TODO: put some real address here once validation present
+        self.assertNotEqual(addr1, addr2)
+
     def test_cmp_address_to_string(self):
         addr1 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
         addr2 = "addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4"
@@ -31,3 +36,17 @@ class TestComparisons(unittest.TestCase):
         addr1 = "addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4"
         addr2 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
         self.assertEqual(addr1, addr2)
+
+    def test_cmp_address_to_bytes(self):
+        addr1 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
+        addr2 = "addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4".encode()
+        self.assertEqual(addr1, addr2)
+
+    def test_cmp_bytes_to_address(self):
+        addr1 = "addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4".encode()
+        addr2 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
+        self.assertEqual(addr1, addr2)
+
+    def test_cmp_address_to_none(self):
+        addr1 = Address("addr1v8fet8gavr6elqt6q50skkjf025zthqu6vr56l5k39sp9aqlvz2g4")
+        self.assertNotEqual(addr1, None)
