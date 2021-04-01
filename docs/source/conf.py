64f7945b495c8081146c18a9ee029eef2b77ac81
Michał Sałaban <michal@salaban.info>
2021-04-01 23:55:45 +0200

    Bump up version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Michał Sałaban'
 author = 'Michał Sałaban'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
