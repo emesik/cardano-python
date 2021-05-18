412079589bb00f09330f398a5787efe2acdac73b
Michał Sałaban <michal@salaban.info>
2021-05-18 22:03:28 +0200

    Add docs on exceptions

--- cardano/backends/walletrest/exceptions.py
+++ cardano/backends/walletrest/exceptions.py
@@ -8,14 +8,20 @@ class WalletRESTException(exceptions.BackendException, exceptions.WalletExceptio
 
 
 class BadRequest(WalletRESTException):
+    """Raised when the underlying REST API returns HTTP code 400."""
     pass
 
 
 class NotFound(WalletRESTException):
+    """Raised when the underlying REST API returns HTTP code 404."""
     pass
 
 
 class RESTServerError(WalletRESTException):
+    """
+    Raised when the underlying REST API returns HTTP code 403 or 500 and the error cannot be
+    handled.
+    """
     pass
 
 
